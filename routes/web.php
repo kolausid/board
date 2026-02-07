9f045527c6256a88bbd59c06efeced1c10f7c73f
Nikolay <nick.zotow@yandex.ru>
2026-02-07 22:52:34 +0300

    name route and logo

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use App\Http\Controllers\BbsController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', [BbsController::class, 'index']);
-Route::get('/{bb}', [BbsController::class, 'detail']);
+Route::get('/', [BbsController::class, 'index'])->name('index');
+Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
 
