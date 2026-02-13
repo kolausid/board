bbe7df7c326c66be57b21edb0484446074f39703
Nikolay <nick.zotow@yandex.ru>
2026-02-13 20:03:07 +0300

    CRUD and validate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\Bb;
 
 class HomeController extends Controller
 {
+    private const BB_VALIDATOR = [
+        'title' => 'required|max:50',
+        'content' => 'required',
+        'price' => 'required|numeric'
+    ];
     /**
      * Create a new controller instance.
      *
@@ -33,11 +38,37 @@ class HomeController extends Controller
     }
     public function store(Request $request)
     {
+        $validated = $request->validate(self::BB_VALIDATOR);
         Auth::user()->bbs()->create([
-            'title' => $request->title, 
-            'content' => $request->content,
-            'price' => $request->price]);
+            'title' => $validated['title'], 
+            'content' => $validated['content'],
+            'price' => $validated['price']
+        ]);
             
+        return redirect()->route('home');
+    }
+    public function edit(Bb $bb)
+    {
+        return view('bb-edit', ['bb' => $bb]);
+    }
+    public function update(Request $request, Bb $bb)
+    {
+        $validated = $request->validate(self::BB_VALIDATOR);
+        $bb->fill([
+            'title' => $validated['title'],
+            'content' => $validated['content'],
+            'price' => $validated['price']
+        ]);
+        $bb->save();
+        return redirect()->route('home');
+    }
+    public function delete(Bb $bb)
+    {
+        return view('bb-delete', ['bb' => $bb]);
+    }
+    public function destroy(Bb $bb)
+    {
+        $bb->delete();
         return redirect()->route('home');
     }
 }
